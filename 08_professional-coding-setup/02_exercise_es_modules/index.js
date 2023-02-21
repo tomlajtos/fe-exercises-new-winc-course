82d9d608d080fed13796faf494b6114d1a5348a2
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-02-21 09:11:13 +0100

    Add Module 08 Exercise 02 and 03
    02 - learn about ES Modules
    03 - split up a project into multiple ES Modules

---
+++ 08_professional-coding-setup/02_exercise_es_modules/index.js
@@ -0,0 +1,21 @@
+// Import multiple things
+import {
+  add,
+  subtract,
+  multiply,
+  divide,
+  square,
+  speedOfLightMetersPerSecond,
+} from "./lib.js";
+
+console.log(add(3, 4));
+
+console.log(subtract(5, 2));
+
+console.log(multiply(10, 4));
+
+console.log(divide(10, 2));
+
+console.log(square(4));
+
+console.log(`The speed of light: ${speedOfLightMetersPerSecond} m/s.`);
