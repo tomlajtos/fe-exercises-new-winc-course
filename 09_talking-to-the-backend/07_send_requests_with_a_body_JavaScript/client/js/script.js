535652e5057a03fa1fa57393d0930c3b8df802d7
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-02-21 21:52:31 +0100

    Add 'sendRequest' function to 10/07E

--- 09_talking-to-the-backend/07_send_requests_with_a_body_JavaScript/client/js/script.js
+++ 09_talking-to-the-backend/07_send_requests_with_a_body_JavaScript/client/js/script.js
@@ -2,7 +2,32 @@
 // them more pure.
 const ROOT_URL = "http://localhost:3000/";
 
-const sendRequest = async (root_url, url_part = "", body = false) => {
+const sendRequest = async (method, root_url, url_part = "", body = false) => {
+    const options = {};
+    let response = {};
+    let json = {};
+
+    if (body && method !== "GET") {
+        options.method = method;
+        options.body = JSON.stringify(body);
+        options.headers = {
+            "Content-Type": "application/json;charset=utf-8"
+        };
+    } else {
+        options.method = "GET";
+    }
+
+    response = await fetch(`${root_url}${url_part}`, options);
+
+    switch (response.status) {
+        case 200:
+        case 201:
+            json = await response.json();
+            break;
+        default: console.error(`Response code: ${response.status}`);
+    }
+
+    return json;
 };
 
 const showReservations = async root_url => {
@@ -12,7 +37,6 @@ const showReservations = async root_url => {
     // we care about.
     reservations = reservations.filter(r => r.date.includes("2024"));
     console.table(reservations);
-    console.log(update.status)
 };
 
 ///////////////////////////////////////////////////////////////////////////////
