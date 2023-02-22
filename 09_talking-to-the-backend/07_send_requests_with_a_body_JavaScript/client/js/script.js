c36fa73b29b03b412d9f40283331049610e6f8fd
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-02-22 13:32:33 +0100

    Add 'updateReserervation', update: 'showRes..', 'addRes..' 10/07E

--- 09_talking-to-the-backend/07_send_requests_with_a_body_JavaScript/client/js/script.js
+++ 09_talking-to-the-backend/07_send_requests_with_a_body_JavaScript/client/js/script.js
@@ -36,34 +36,83 @@ const showReservations = async root_url => {
     // Only show reservations in 2024 to make it easier to see the reservations
     // we care about.
     reservations = reservations.filter(r => r.date.includes("2024"));
+    console.group("Reservations");
     console.table(reservations);
+    console.groupEnd();
 };
 
 const addReservation = async function(root_url, body) {
-    console.log(`Making a new reservation for ${body.name} date:${body.date} time:${body.time}`);
+    console.group(`Making a new reservation for ${body.name}, date: ${body.date}, time: ${body.time}...`);
 
     let newRes = await sendRequest("POST", root_url, "reservations", body);
+    let resSuccess = Object.keys(newRes).length ? true : false;
 
-    console.table(newRes)
-    Object.keys(newRes).length ? console.log("res successful:", true) : console.log("res successful:", false);
-    return newRes;
+    if (resSuccess) {
+        console.table(newRes);
+        console.groupEnd();
+        console.log(`Reservation sucessful:%c ${resSuccess}, ID: ${newRes.id}`, "color: green");
+        return newRes
+    }
+    console.groupEnd();
+    console.log(`Reservation sucessful:%c ${resSuccess}, ID: ${newRes.id}`, "color: red");
+}
+
+const updateReservation = async function(method, root_url, id, body) {
+    const endpoint = `reservations/${id}`;
+    let reservation = await sendRequest("GET", root_url, endpoint);
+    console.group(`Updating reservation ID: ${reservation.id}...`);
+    console.group("Current reservation:");
+    console.table(reservation);
+    console.groupEnd();
+
+    console.group("Updated reservation:");
+    console.table(Object.keys(reservation)
+        .reduce((currRes, key) => {
+            if (key in body) {
+                currRes[key] = body[key];
+            }
+            else {
+                currRes[key] = reservation[key];
+            }
+            return currRes;
+        }, {})
+    );
+    console.groupEnd();
+
+    let update = await sendRequest(method, root_url, endpoint, body);
+    let updateSuccess = Object.keys(update).length ? true : false;
+    if (updateSuccess) {
+        console.groupEnd();
+        console.log(`Update successful:%c ${updateSuccess}`, "color: green");
+        return update;
+    }
+    console.groupEnd();
+    console.log(`Update successful:%c ${updateSuccess}`, "color: red");
+
 }
 ///////////////////////////////////////////////////////////////////////////////
 // Show all reservations
 await showReservations(ROOT_URL);
-
 ///////////////////////////////////////////////////////////////////////////////
 // Create a new reservation.
 const huanRes = { "name": "Sam Huan", "people": 6, "date": "12/12/2024", "time": "20:00" };
 let huanResResult = await addReservation(ROOT_URL, huanRes);
-console.log("Huan reservation ID:", huanResResult.id);
-await showReservations(ROOT_URL);
+// await showReservations(ROOT_URL);
 
 //////////////////////////////////////////////////////////////////////////////
 // Create another new reservation.
+const boRes = { "name": "Bim Bo", "people": 2, "date": "22/10/2024", "time": "18:00" };
+let boResResult = await addReservation(ROOT_URL, boRes);
+await showReservations(ROOT_URL);
 
 ///////////////////////////////////////////////////////////////////////////////
-// Update Bim Bo's reservation using PUT.
+// Update Sam Huan's reservation using PUT.
+const huanResUpdate = { ...huanResResult, "name": "Mr. Sam Huan", "time": "21:00" };
+let huanResUpdateResult = await updateReservation("PUT", ROOT_URL, huanResResult.id, huanResUpdate);
+// await showReservations(ROOT_URL);
 
 ///////////////////////////////////////////////////////////////////////////////
-// Update Sam Huan's reservation using PATCH.
+// Update Bim Bo's reservation using PACH.
+const boResUpdate = { "people": 4, "date": "22/10/2024", "time": "19:00" };
+let boResUpdateResult = await updateReservation("PATCH", ROOT_URL, boResResult.id, boResUpdate);
+await showReservations(ROOT_URL);
