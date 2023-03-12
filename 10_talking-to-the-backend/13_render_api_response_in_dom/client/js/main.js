412736bf9c47273b157bfcad8eedffe2fa6f09ef
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-03-12 17:04:19 +0100

    Comment out io module test in main.js 10/13

--- 10_talking-to-the-backend/13_render_api_response_in_dom/client/js/main.js
+++ 10_talking-to-the-backend/13_render_api_response_in_dom/client/js/main.js
@@ -4,35 +4,35 @@ import { addItem, getItem, getItems, updateItem, deleteItem } from "./io.js";
 addEventListeners();
 
 // test imported io functions
-console.log("Flavours:", await getItems("flavours"));
-
-const vanilla = await getItem("flavours", 1);
-console.log("Vanilla:", vanilla);
-
-const coffee = {
-	"name": "coffee",
-	"price": 2.1
-};
-
-const addCoffeeFlavour = await addItem("flavours", coffee);
-console.log("new flavour added:", addCoffeeFlavour);
+// console.log("Flavours:", await getItems("flavours"));
+//
+// const vanilla = await getItem("flavours", 1);
+// console.log("Vanilla:", vanilla);
+//
+// const coffee = {
+// 	"name": "coffee",
+// 	"price": 2.1
+// };
+//
+// const addCoffeeFlavour = await addItem("flavours", coffee);
+// console.log("new flavour added:", addCoffeeFlavour);
 
 // console.log("Flavours:", await getItems("flavours"));
 
-const newCoffee = {
-	"name": "coffee",
-	"price": 1.95,
-	"id": 4
-};
-
-const updatedCoffee = await updateItem("flavours", addCoffeeFlavour.id, newCoffee);
-console.log("coffee flavour updated:", updatedCoffee); 
-
-console.log("Flavours:", await getItems("flavours"));
-
-deleteItem("flavours", addCoffeeFlavour.id);
-console.log(`deleting flavour: ${addCoffeeFlavour.name}, id: ${addCoffeeFlavour.id}`);
-
-console.log("Flavours:", await getItems("flavours"));
-console.log("Customers:", await getItems("customers"));
-console.log("Orders:", await getItems("orders"));
+// const newCoffee = {
+// 	"name": "coffee",
+// 	"price": 1.95,
+// 	"id": 4
+// };
+//
+// const updatedCoffee = await updateItem("flavours", addCoffeeFlavour.id, newCoffee);
+// console.log("coffee flavour updated:", updatedCoffee); 
+//
+// console.log("Flavours:", await getItems("flavours"));
+//
+// deleteItem("flavours", addCoffeeFlavour.id);
+// console.log(`deleting flavour: ${addCoffeeFlavour.name}, id: ${addCoffeeFlavour.id}`);
+//
+// console.log("Flavours:", await getItems("flavours"));
+// console.log("Customers:", await getItems("customers"));
+// console.log("Orders:", await getItems("orders"));
