98b8f2c1dd6adf6b43b60960da43c0b321b7ec79
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-03-20 18:05:38 +0100

    Add props to DrinkButtons component and add utils/data.js to project

--- 11_react-basics/04_props_exercise/my-first-app/src/App.jsx
+++ 11_react-basics/04_props_exercise/my-first-app/src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './App.css';
 import {DrinkButtons} from './components/DrinkButtons.jsx';
+import { coffee, tea } from './utils/data';
 
 export const App = () => {
 	const greeting = "Welcome to our cafe!";
@@ -8,7 +9,7 @@ export const App = () => {
 	return (
 		<div className="App">
 			<h1>{greeting}</h1>
-			<DrinkButtons/>
+			<DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
 		</div>
 	);
 }
--- 11_react-basics/04_props_exercise/my-first-app/src/components/DrinkButtons.jsx
+++ 11_react-basics/04_props_exercise/my-first-app/src/components/DrinkButtons.jsx
@@ -1,16 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './DrinkButtons.css';
 
-export const DrinkButtons = () => {
-	const tea = "Tea";
-	const coffee = "Coffee";
-
+export const DrinkButtons = ({drinkOne, drinkTwo}) => {
 	return (
 		<>
 			<h2>Would you like to have tea or coffee?</h2>
 			<div className="button-group">
-				<button className="button">{tea}</button>
-				<button className="button">{coffee}</button>
+				<button className="button">{drinkOne}</button>
+				<button className="button">{drinkTwo}</button>
 			</div>
 		</>
 	);
