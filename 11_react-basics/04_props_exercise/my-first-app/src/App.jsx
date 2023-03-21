4c4635464756ef0efc7434d389c792c61f5e012a
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-03-21 14:36:31 +0100

    Use 'DrinkChoice' in 'App'

--- 11_react-basics/04_props_exercise/my-first-app/src/App.jsx
+++ 11_react-basics/04_props_exercise/my-first-app/src/App.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope */
+// interactive drink coice will come in future exercise (R. state)
 import './App.css';
-import {DrinkButtons} from './components/DrinkButtons.jsx';
-import { coffee, tea } from './utils/data';
+import { DrinkButtons } from './components/DrinkButtons.jsx';
+import { DrinkChoice } from './components/DrinkChoice.jsx';
+import { coffee, tea } from './utils/data.js';
 
 export const App = () => {
 	const greeting = "Welcome to our cafe!";
+	const userDrink = coffee;
 
 	return (
 		<div className="App">
 			<h1>{greeting}</h1>
 			<DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+			<DrinkChoice drink={userDrink} />
 		</div>
 	);
 }
