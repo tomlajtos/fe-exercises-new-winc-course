2e5e53170579065ca98c89608d9fd89437686c1d
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-03-24 12:37:37 +0100

    Use 'DrinkSearch' component

--- 11_react-basics/06_state_exercise/my-first-app/src/App.jsx
+++ 11_react-basics/06_state_exercise/my-first-app/src/App.jsx
@@ -4,17 +4,19 @@ import { useState } from 'react';
 import './App.css';
 import { DrinkButtons } from './components/DrinkButtons.jsx';
 import { DrinkChoice } from './components/DrinkChoice.jsx';
+import { DrinkSearch } from './components/DrinkSearch';
 import { coffee, tea } from './utils/data.js';
 
 export const App = () => {
 	const greeting = "Welcome to our cafe!";
-	const [userDrink, setUserDrink] = useState(tea);
+	const [userDrink, setUserDrink] = useState();
 
 	return (
 		<div className="App">
 			{userDrink ? (<DrinkChoice drink={userDrink} />) : (
 				<>
 					<h1>{greeting}</h1>
+					<DrinkSearch />
 					<DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
 				</>
 			)}
