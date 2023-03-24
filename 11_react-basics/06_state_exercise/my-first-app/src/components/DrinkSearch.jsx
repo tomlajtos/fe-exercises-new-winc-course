214c4603362d01cbddaa1bf6960ef469b5602041
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-03-24 12:36:40 +0100

    Add components for text input and drink search

---
+++ 11_react-basics/06_state_exercise/my-first-app/src/components/DrinkSearch.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { TextInput } from './TextInput.jsx';
+
+export const DrinkSearch = () => {
+	const [searchField, setSearchField] = useState("americano");
+
+	return (
+		<div className="drink-search">
+			<label>You can search for available drinks here:</label>
+			<TextInput />
+			{searchField && (<p>Search results for: {searchField}</p>)}
+		</div>
+	);
+};
---
+++ 11_react-basics/06_state_exercise/my-first-app/src/components/TextInput.jsx
@@ -0,0 +1,5 @@
+export const TextInput = () => {
+	return (
+		<input className="text-input" type="text" placeholder="Type the name of the drink..." />
+	);
+}
