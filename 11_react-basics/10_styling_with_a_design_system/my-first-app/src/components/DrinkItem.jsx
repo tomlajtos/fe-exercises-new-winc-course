02e5c43dec31ff5a17cb1cf535d14075065d6e89
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-18 13:11:08 +0200

    refactor: Style DrinkItem with Chakra, del .css

--- 11_react-basics/10_styling_with_a_design_system/my-first-app/src/components/DrinkItem.jsx
+++ 11_react-basics/10_styling_with_a_design_system/my-first-app/src/components/DrinkItem.jsx
@@ -1,12 +1,26 @@
-import './DrinkItem.css';
+import { Center, Image, Text } from "@chakra-ui/react";
 
-export const DrinkItem = ({ drink, clickFn}) => {
-	return (
-		<>
-			<button className="drink-item" onClick={()=>clickFn(drink)}>
-				<img src={drink.imgUrl} alt={drink.alt} />
-				<p>{drink.name}</p>
-			</button>
-		</>
-	);
+export const DrinkItem = ({ drink, clickFn }) => {
+  return (
+    <>
+      <Center
+        my={2}
+        p={4}
+        w={250}
+        border="1px"
+        borderColor="transparent"
+        boxSizing="border-box"
+        _hover={{
+          border: "1px",
+          borderColor: "purple.100",
+          borderRadius: "lg",
+          cursor: "pointer",
+        }}
+        onClick={() => clickFn(drink)}
+      >
+        <Image w={50} h={50} mr={4} src={drink.imgUrl} alt={drink.alt} />
+        <Text fontSize="lg">{drink.name}</Text>
+      </Center>
+    </>
+  );
 };
