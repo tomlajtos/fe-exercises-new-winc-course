10e7ae8b91078901a9758ddf7f9fe07514330b73
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-18 10:11:57 +0200

    refactor: use Chakra's "Input" to style <TextInput>

--- 11_react-basics/10_styling_with_a_design_system/my-first-app/src/components/DrinkSearch.jsx
+++ 11_react-basics/10_styling_with_a_design_system/my-first-app/src/components/DrinkSearch.jsx
@@ -1,27 +1,36 @@
-/* eslint-disable react/react-in-jsx-scope */
-import { useState } from 'react';
-import { availableDrinks } from '../utils/data.js';
-import './DrinkSearch.css';
-import { TextInput } from './ui/TextInput.jsx';
-import { DrinkList } from './DrinkList.jsx';
+import { useState } from "react";
+import { availableDrinks } from "../utils/data.js";
+import "./DrinkSearch.css";
+import { TextInput } from "./ui/TextInput.jsx";
+import { DrinkList } from "./DrinkList.jsx";
 
 export const DrinkSearch = ({ clickFn }) => {
-	const [searchField, setSearchField] = useState("");
-	const handleChange = (event) => setSearchField(event.target.value) 
-	const matchedDrinks = availableDrinks.filter(drink => { return drink.name.toLowerCase().includes(searchField.toLowerCase()) });
+  const [searchField, setSearchField] = useState("");
+  const handleChange = (event) => setSearchField(event.target.value);
+  const matchedDrinks = availableDrinks.filter((drink) => {
+    return drink.name.toLowerCase().includes(searchField.toLowerCase());
+  });
 
-	return (
-		<div className="drink-search">
-			<label>You can search for available drinks here:</label>
-			<TextInput changeFn={handleChange} />
-			{searchField ?
-				<>
-					<p>Search results: </p>
-					<DrinkList className="list" drinks={matchedDrinks} clickFn={clickFn} />
-				</>
-				:
-				<DrinkList className="list" drinks={availableDrinks} clickFn={clickFn} />}
-		</div>
-	);
+  return (
+    <div className="drink-search">
+      <label>You can search for available drinks here:</label>
+      <TextInput changeFn={handleChange} mb={8} w={400} />
+      {searchField ? (
+        <>
+          <p>Search results: </p>
+          <DrinkList
+            className="list"
+            drinks={matchedDrinks}
+            clickFn={clickFn}
+          />
+        </>
+      ) : (
+        <DrinkList
+          className="list"
+          drinks={availableDrinks}
+          clickFn={clickFn}
+        />
+      )}
+    </div>
+  );
 };
-
--- 11_react-basics/10_styling_with_a_design_system/my-first-app/src/components/ui/TextInput.jsx
+++ 11_react-basics/10_styling_with_a_design_system/my-first-app/src/components/ui/TextInput.jsx
@@ -1,7 +1,15 @@
-import './TextInput.css';
+import { Input } from "@chakra-ui/react";
 
-export const TextInput = ({changeFn}) => {
-	return (
-		<input className="text-input" type="text" placeholder="Type the name of the drink..." onChange={changeFn} />
-	);
-}
+export const TextInput = ({ changeFn, ...props }) => {
+  return (
+    <Input
+      variant="outline"
+      focusBorderColor="purple.500"
+      className="text-input"
+      type="text"
+      placeholder="Type the name of the drink..."
+      onChange={changeFn}
+      {...props}
+    />
+  );
+};
