c71801dd6046b44c4d6a8c7fd52afd59e972089b
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-24 11:30:21 +0200

    refactor: use P.ItemCard, show all p.Items, use Chakra

--- 11_react-basics/10_03_styling_with_chakra__portfolio_ex/portfolio-exercise/src/components/PortfolioPage.jsx
+++ 11_react-basics/10_03_styling_with_chakra__portfolio_ex/portfolio-exercise/src/components/PortfolioPage.jsx
@@ -1,11 +1,21 @@
-export const PortfolioPage = () => {
+import { Container, Wrap, Heading, Text } from "@chakra-ui/react";
+import { PortfolioItemCard } from "./PortfolioItemCard";
+
+export const PortfolioPage = ({ items, handleSelection }) => {
   return (
-    <>
-      <div>
-        <hr />
-        <h2>This is the Portfolio Page (WIP)</h2>
-        <hr />
-      </div>
-    </>
+    <Container w="100%" minW="80vw" minH="100vh" bg="gray.50">
+      <Heading fontSize={"2xl"}>This is the Portfolio Page (WIP)</Heading>
+      <Wrap>
+        {items.map((item) => {
+          return (
+            <PortfolioItemCard
+              key={item.id}
+              item={item}
+              handleSelection={handleSelection}
+            />
+          );
+        })}
+      </Wrap>
+    </Container>
   );
 };
