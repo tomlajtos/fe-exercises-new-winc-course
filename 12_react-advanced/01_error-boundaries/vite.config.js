d4e8b37e6b635458234228afb2178af6b95cb24a
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-07-26 21:54:35 +0200

    Setup Vitest and add files to test testing

--- 12_react-advanced/01_error-boundaries/vite.config.js
+++ 12_react-advanced/01_error-boundaries/vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "./tests/setup.js",
+  },
   server: {
     open: false,
     port: 3000,
