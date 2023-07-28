9c9156d8bb3f615f0b5553fb7e4434d7774731da
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-07-28 23:43:59 +0200

    Add controlled form

--- 12_react-advanced/02_forms_create-a-simple-controlled-form/src/App.jsx
+++ 12_react-advanced/02_forms_create-a-simple-controlled-form/src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const addQuote = (e) => {
+    e.preventDefault();
+    setQuotes([{ quote, author }].concat(quotes)); // setQuotes([{quote,author}, ...quotes])
+    setQuote("");
+    setAuthor("");
+  };
+
   return (
     <div className="App">
       <h1>Our beautiful form will be here:</h1>
+      <form onSubmit={addQuote}>
+        <label>
+          <textarea
+            value={quote}
+            placeholder={"Quote..."}
+            onChange={(e) => setQuote(e.target.value)}
+          ></textarea>
+        </label>
+        <label>
+          <input
+            value={author}
+            placeholder={"Author"}
+            onChange={(e) => setAuthor(e.target.value)}
+          ></input>
+        </label>
+        <button type="submit">Add Quote</button>
+      </form>
       <h1>Quotes:</h1>
       <div className="quote">
         <p>
