61f3374658a8bdba20d66c7d391f2408ceaf76f2
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-07-28 22:21:00 +0200

    Add starter project for 12_02, Forms

---
+++ 12_react-advanced/02_forms_create-a-simple-controlled-form/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Our beautiful form will be here:</h1>
+      <h1>Quotes:</h1>
+      <div className="quote">
+        <p>
+          My biggest fear is that people will attribute fake quotes to me and
+          millions of morons on the internet will believe it.
+        </p>
+        - <span>Albert Einstein</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
