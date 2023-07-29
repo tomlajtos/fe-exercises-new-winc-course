4ac1bae86d0778d33d61098fb2ce49e2f31537a5
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-07-29 10:44:58 +0200

    Render quotes

--- 12_react-advanced/02_forms_create-a-simple-controlled-form/src/App.jsx
+++ 12_react-advanced/02_forms_create-a-simple-controlled-form/src/App.jsx
@@ -1,45 +1,59 @@
 import { useState } from "react";
 
 function App() {
-  const [quotes, setQuotes] = useState([]);
+  const [quotes, setQuotes] = useState([
+    {
+      id: "q_1",
+      quote:
+        " My biggest fear is that people will attribute fake quotes to me and millions of morons on the internet will believe it.",
+      author: "Albert Einstein",
+    },
+  ]);
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
 
   const addQuote = (e) => {
     e.preventDefault();
-    setQuotes([{ quote, author }].concat(quotes)); // setQuotes([{quote,author}, ...quotes])
+    const id = `q_${quotes.length + 1}`;
+    setQuotes([{ id: id, quote: quote, author: author }].concat(quotes)); // alt: setQuotes([{id:id, quote:quote, author:author}, ...quotes]);
     setQuote("");
     setAuthor("");
   };
 
   return (
     <div className="App">
-      <h1>Our beautiful form will be here:</h1>
+      <h1>Add your favorite quotes:</h1>
       <form onSubmit={addQuote}>
         <label>
           <textarea
             value={quote}
-            placeholder={"Quote..."}
+            placeholder={'"Quote..."'}
             onChange={(e) => setQuote(e.target.value)}
+            rows={5}
+            cols={30}
           ></textarea>
         </label>
         <label>
           <input
             value={author}
-            placeholder={"Author"}
+            placeholder={"Author..."}
+            style={{ width: "200px" }}
             onChange={(e) => setAuthor(e.target.value)}
           ></input>
         </label>
         <button type="submit">Add Quote</button>
       </form>
       <h1>Quotes:</h1>
-      <div className="quote">
-        <p>
-          My biggest fear is that people will attribute fake quotes to me and
-          millions of morons on the internet will believe it.
-        </p>
-        - <span>Albert Einstein</span>
-      </div>
+      {quotes.map((q, i, a) => {
+        return (
+          <div className="quote" key={q.id}>
+            <p>{q.quote}</p>- <span>{q.author}</span>
+            {a.length !== i + 1 && (
+              <hr className="quote-hr" style={{ opacity: 0.1 }} />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
