6bef709c2c31c25fd253b95ebb794d6577a0a056
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-01 21:36:09 +0200

    Add state, and borrow and return functions that update the state

--- 12_react-advanced/12_03_01_Component-composition/src/Library.jsx
+++ 12_react-advanced/12_03_01_Component-composition/src/Library.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+import { collection } from "./collection";
+
 export default () => {
+  const [books, setBooks] = useState(collection.books);
+
+  const borrowBook = (id) => {
+    setBooks(() =>
+      books.map((book) => {
+        if (id === book.id) {
+          return { ...book, available: false };
+        }
+        return book;
+      }),
+    );
+  };
+
+  const returnBook = (id) => {
+    setBooks(() =>
+      books.map((book) => {
+        if (id === book.id) {
+          return { ...book, available: true };
+        }
+        return book;
+      }),
+    );
+  };
+
   return (
     <div className="App">
       <h1>Prop Drilling and Component Composition</h1>
