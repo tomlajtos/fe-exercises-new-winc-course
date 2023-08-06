aeb0c8638ef52c294706979ab73c0c94e40a7b01
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-06 22:23:39 +0200

    Create LibraryContext and add it to Library component

--- 12_react-advanced/04_02_Create_and_consume_context/src/Library.jsx
+++ 12_react-advanced/04_02_Create_and_consume_context/src/Library.jsx
@@ -1,25 +1,28 @@
-import { useState } from "react";
+import { useState, usContext } from "react";
 import { collection } from "./collection";
+import { LibraryContext } from "./LibraryContext";
 import { Books } from "./Books";
 
 function Library() {
   const [books, setBooks] = useState(collection.books);
   const borrowBook = (id) => {
     const newBooks = books.map((book) =>
-      book.id === id ? { ...book, available: false } : book
+      book.id === id ? { ...book, available: false } : book,
     );
     return setBooks(newBooks);
   };
   const returnBook = (id) => {
     const newBooks = books.map((book) =>
-      book.id === id ? { ...book, available: true } : book
+      book.id === id ? { ...book, available: true } : book,
     );
     return setBooks(newBooks);
   };
   return (
     <div className="App">
-      <h1>Library</h1>
-      <Books books={books} borrowBook={borrowBook} returnBook={returnBook} />
+      <LibraryContext.Provider value={{ books, borrowBook, returnBook }}>
+        <h1>Library</h1>
+        <Books />
+      </LibraryContext.Provider>
     </div>
   );
 }
