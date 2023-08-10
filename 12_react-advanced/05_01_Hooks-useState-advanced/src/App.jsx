87fa6c57e76693e511071fd7c291d296d2110cba
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-10 15:44:18 +0200

    Add state (name) and use updater function to set new state

--- 12_react-advanced/05_01_Hooks-useState-advanced/src/App.jsx
+++ 12_react-advanced/05_01_Hooks-useState-advanced/src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
+crateInitialState = () => {};
+
 const App = () => {
-	return (
-		<div className="App">
-			<h1>React Hooks Exercise Starter</h1>
-		</div>
-	);
+  const [name, setName] = useState("eman ym");
+
+  const handleClick = () => {
+    setName((n) => n + "o");
+    console.log(name);
+    setName((n) => n + "o");
+    console.log(name);
+  };
+
+  return (
+    <div className="App">
+      <h1>React Hooks Exercise Starter</h1>
+      <button type={"button"} onClick={handleClick}>
+        Change name
+      </button>
+      <p>{name}</p>
+    </div>
+  );
 };
 
 export default App;
