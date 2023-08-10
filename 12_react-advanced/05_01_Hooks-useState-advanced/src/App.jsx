2e95db3874b84444f7ed66b827116fea7b40d026
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-10 15:57:42 +0200

    Add state (age) and set init value with a function, increment /w button

--- 12_react-advanced/05_01_Hooks-useState-advanced/src/App.jsx
+++ 12_react-advanced/05_01_Hooks-useState-advanced/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-crateInitialState = () => {};
+const createInitialState = () => new Date().getFullYear() - 1945;
 
 const App = () => {
   const [name, setName] = useState("eman ym");
-
+  const [age, setAge] = useState(createInitialState);
+  console.log(age);
   const handleClick = () => {
     setName((n) => n + "o");
     console.log(name);
@@ -12,13 +13,21 @@ const App = () => {
     console.log(name);
   };
 
+  const addOneYear = () => {
+    setAge((a) => a + 1);
+  };
+
   return (
     <div className="App">
       <h1>React Hooks Exercise Starter</h1>
+      <p>Name: {name}</p>
       <button type={"button"} onClick={handleClick}>
         Change name
       </button>
-      <p>{name}</p>
+      <p>Age: {age}</p>
+      <button type={"button"} onClick={addOneYear}>
+        Birthday
+      </button>
     </div>
   );
 };
