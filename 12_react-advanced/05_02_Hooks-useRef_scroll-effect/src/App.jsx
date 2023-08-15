4d5f4809700e930991cb336e481294fa70723ce6
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-15 14:01:00 +0200

    Add 12/05-02: Hooks > useRef, exercise starter

---
+++ 12_react-advanced/05_02_Hooks-useRef_scroll-effect/src/App.jsx
@@ -0,0 +1,13 @@
+export const App = () => {
+  return (
+    <>
+      <div className="fullscreen-height">
+        <h1>Ref exercise starter</h1>
+        <button> Click to scroll </button>
+      </div>
+      <div className="fullscreen-height lightblue-background">
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
