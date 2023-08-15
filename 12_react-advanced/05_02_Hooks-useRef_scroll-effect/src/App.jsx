421b567c49fe8e57ff08d2f9c951202cbb52da1c
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-15 14:35:06 +0200

    Add refs to both divs, use scrollIntoView() to  scroll back and forth

--- 12_react-advanced/05_02_Hooks-useRef_scroll-effect/src/App.jsx
+++ 12_react-advanced/05_02_Hooks-useRef_scroll-effect/src/App.jsx
@@ -1,12 +1,27 @@
+import { useRef } from "react";
+
 export const App = () => {
+  const helloRef = useRef(null);
+  const homeRef = useRef(null);
+  const executeScroll = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
-      <div className="fullscreen-height">
-        <h1>Ref exercise starter</h1>
-        <button> Click to scroll </button>
+      <div ref={homeRef} className="fullscreen-height">
+        <h1>Ref exercise</h1>
+        <button onClick={() => executeScroll(helloRef)}>
+          {" "}
+          Click to scroll{" "}
+        </button>
       </div>
-      <div className="fullscreen-height lightblue-background">
+      <div ref={helloRef} className="fullscreen-height lightblue-background">
         <h1>Hello</h1>
+        <button onClick={() => executeScroll(homeRef)}>
+          {" "}
+          Click to scroll{" "}
+        </button>
       </div>
     </>
   );
