797382bbb17bc2c28d5bcf01f887ef0ae688d4d9
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-15 15:00:21 +0200

    Remove white space strings (formatting error)

--- 12_react-advanced/05_02_Hooks-useRef_scroll-effect/src/App.jsx
+++ 12_react-advanced/05_02_Hooks-useRef_scroll-effect/src/App.jsx
@@ -3,6 +3,7 @@ import { useRef } from "react";
 export const App = () => {
   const helloRef = useRef(null);
   const homeRef = useRef(null);
+
   const executeScroll = (ref) => {
     ref.current.scrollIntoView({ behavior: "smooth" });
   };
@@ -11,17 +12,11 @@ export const App = () => {
     <>
       <div ref={homeRef} className="fullscreen-height">
         <h1>Ref exercise</h1>
-        <button onClick={() => executeScroll(helloRef)}>
-          {" "}
-          Click to scroll{" "}
-        </button>
+        <button onClick={() => executeScroll(helloRef)}>Click to scroll</button>
       </div>
       <div ref={helloRef} className="fullscreen-height lightblue-background">
         <h1>Hello</h1>
-        <button onClick={() => executeScroll(homeRef)}>
-          {" "}
-          Click to scroll{" "}
-        </button>
+        <button onClick={() => executeScroll(homeRef)}>Click to scroll</button>
       </div>
     </>
   );
