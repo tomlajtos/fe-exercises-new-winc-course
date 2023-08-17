a9f7fda546eac11c8c4c3a3d6b202ccd2a5b2af2
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-17 17:32:48 +0200

    Add states, buttons for peaople, divs for people

--- 12_react-advanced/05_03_Hooks-useEffect_fetching-data/src/App.jsx
+++ 12_react-advanced/05_03_Hooks-useEffect_fetching-data/src/App.jsx
@@ -1,9 +1,30 @@
+/*eslint no-unused-vars: 0 */
+import { useEffect, useState } from "react";
+// import fetchers > backend emulation
+import { fetchPeople, fetchPerson } from "./fetchers.js";
+
 const App = () => {
-	return (
-		<div className="App">
-			<h1>React Hooks Exercise Starter</h1>
-		</div>
-	);
+  const [people, setPeople] = useState([]);
+  const [person, setPerson] = useState(null);
+  const [id, setId] = useState(null);
+
+  return (
+    <div className="App">
+      <h1>React Hooks Exercise Starter</h1>
+      {people.map((person) => (
+        <button key={person.id} onClick={() => setId(person.id)}>
+          person.name
+        </button>
+      ))}
+      {person && (
+        <div>
+          <h2>{person.name}</h2>
+          <p>Age: {person.age}</p>
+          <p>Hobbies: {person.hobbies}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
