94d270af5c6c86b8732d4abe78b32caefd44c2e2
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-23 22:41:32 +0200

    Add useEffect to set state value in localStorage

--- 12_react-advanced/05_04_Hooks-Custom-Hooks_useLocalStorage-hook/src/App.jsx
+++ 12_react-advanced/05_04_Hooks-Custom-Hooks_useLocalStorage-hook/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  // use a setter function to get value that belongs to the KEY 'namej'
+  // use a setter function to get value that belongs to the KEY 'name'
   // set a def value as well with the `||` operator
   const [name, setName] = useState(
     () => window.localStorage.getItem("name") || "Name-o",
   );
+
+  // use useEffect to update 'name' in localStorage
+  // since our component has to be synced with an external system (Web Storage API)
+  useEffect(() => {
+    window.localStorage.setItem("name", name);
+  }, [name]);
 
   return (
     <div className="App">
