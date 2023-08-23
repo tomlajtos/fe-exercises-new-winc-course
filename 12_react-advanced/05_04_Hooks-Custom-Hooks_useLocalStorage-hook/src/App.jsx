804c7cc25206c5d983a42579cf8a6dec5b73ca7f
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-23 23:29:23 +0200

    Add a second state/value to localStorage

--- 12_react-advanced/05_04_Hooks-Custom-Hooks_useLocalStorage-hook/src/App.jsx
+++ 12_react-advanced/05_04_Hooks-Custom-Hooks_useLocalStorage-hook/src/App.jsx
@@ -2,11 +2,14 @@ import { useLocalStorage } from "./useLocalStorage.js";
 
 const App = () => {
   const [name, setName] = useLocalStorage("name", "Name-o");
+  const [subject, setSubject] = useLocalStorage("subject", "React Hooks");
 
   return (
     <div className="App">
       <h1>Welcome {name}!</h1>
       <input type={"text"} onChange={(e) => setName(e.target.value)}></input>
+      <p>Continue learning {subject}</p>
+      <input type={"text"} onChange={(e) => setSubject(e.target.value)}></input>
     </div>
   );
 };
