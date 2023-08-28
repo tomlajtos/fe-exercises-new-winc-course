912d9d2a8a4312dcb9d1921806e915507ef51437
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-28 21:44:25 +0200

    refactor(Lib.Context.Prov): use useReducer instead of useState

--- 12_react-advanced/05_05_Hooks-useReducer_optional-exercise/src/LibraryContext.jsx
+++ 12_react-advanced/05_05_Hooks-useReducer_optional-exercise/src/LibraryContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useReducer } from "react";
 import { collection } from "./collection";
 
 export const LibraryContext = createContext({});
@@ -6,18 +6,27 @@ export const LibraryContext = createContext({});
 LibraryContext.displayName = "LibraryContext";
 
 export const LibraryContextProvider = ({ children }) => {
-  const [books, setBooks] = useState(collection.books);
+  // const [books, setBooks] = useState(collection.books);
+  const [books, dispatch] = useReducer(libraryReducer, collection.books);
+
+  // const borrowBook = (id) => {
+  //   const newBooks = books.map((book) =>
+  //     book.id === id ? { ...book, available: false } : book,
+  //   );
+  //   return setBooks(newBooks);
+  // };
   const borrowBook = (id) => {
-    const newBooks = books.map((book) =>
-      book.id === id ? { ...book, available: false } : book
-    );
-    return setBooks(newBooks);
+    dispatch({ type: "borrow_book", id });
   };
+
+  // const returnBook = (id) => {
+  //   const newBooks = books.map((book) =>
+  //     book.id === id ? { ...book, available: true } : book,
+  //   );
+  //   return setBooks(newBooks);
+  // };
   const returnBook = (id) => {
-    const newBooks = books.map((book) =>
-      book.id === id ? { ...book, available: true } : book
-    );
-    return setBooks(newBooks);
+    dispatch({ type: "return_book", id });
   };
 
   return (
@@ -33,4 +42,34 @@ export const useLibrary = () => {
     throw new Error("useLibrary must be used within a LibraryContextProvider");
   }
   return context;
+};
+
+const libraryReducer = (state, action) => {
+  switch (action.type) {
+    case "borrow_book": {
+      return state.map((item) => {
+        if (item.id === action.id) {
+          return {
+            ...item,
+            available: false,
+          };
+        }
+        return item;
+      });
+    }
+    case "return_book": {
+      return state.map((item) => {
+        if (item.id === action.id) {
+          return {
+            ...item,
+            available: true,
+          };
+        }
+        return item;
+      });
+    }
+    default: {
+      return state;
+    }
+  }
 };
