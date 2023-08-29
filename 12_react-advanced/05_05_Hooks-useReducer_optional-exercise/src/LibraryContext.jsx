3177b33166ef7435c8cd7d0e50a063628bed4258
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-29 22:31:22 +0200

    Add 'add_book' action to reducer and LibraryContextProvider

--- 12_react-advanced/05_05_Hooks-useReducer_optional-exercise/src/LibraryContext.jsx
+++ 12_react-advanced/05_05_Hooks-useReducer_optional-exercise/src/LibraryContext.jsx
@@ -1,54 +1,9 @@
-import { createContext, useContext, useState, useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 import { collection } from "./collection";
 
 export const LibraryContext = createContext({});
 
 LibraryContext.displayName = "LibraryContext";
-
-export const LibraryContextProvider = ({ children }) => {
-  // const [books, setBooks] = useState(collection.books);
-  const [books, dispatch] = useReducer(libraryReducer, collection.books);
-
-  // const borrowBook = (id) => {
-  //   const newBooks = books.map((book) =>
-  //     book.id === id ? { ...book, available: false } : book,
-  //   );
-  //   return setBooks(newBooks);
-  // };
-  const borrowBook = (id) => {
-    dispatch({ type: "borrow_book", id });
-  };
-
-  // const returnBook = (id) => {
-  //   const newBooks = books.map((book) =>
-  //     book.id === id ? { ...book, available: true } : book,
-  //   );
-  //   return setBooks(newBooks);
-  // };
-  const returnBook = (id) => {
-    dispatch({ type: "return_book", id });
-  };
-
-  const removeBook = (id) => {
-    dispatch({ type: "remove_book", id });
-  };
-
-  return (
-    <LibraryContext.Provider
-      value={{ books, borrowBook, returnBook, removeBook }}
-    >
-      {children}
-    </LibraryContext.Provider>
-  );
-};
-
-export const useLibrary = () => {
-  const context = useContext(LibraryContext);
-  if (!context) {
-    throw new Error("useLibrary must be used within a LibraryContextProvider");
-  }
-  return context;
-};
 
 const libraryReducer = (state, action) => {
   switch (action.type) {
@@ -77,8 +32,56 @@ const libraryReducer = (state, action) => {
     case "remove_book": {
       return state.filter((item) => item.id !== action.id);
     }
+    case "add_book": {
+      const lastItem = state[state.length - 1];
+      const newId = lastItem ? lastItem.id + 1 : 1;
+      const item = {
+        id: newId,
+        title: action.title,
+        author: action.author,
+        category: action.category.toLowerCase(),
+        available: true,
+      };
+      return [...state, item];
+    }
     default: {
       return state;
     }
   }
+};
+
+export const LibraryContextProvider = ({ children }) => {
+  const [books, dispatch] = useReducer(libraryReducer, collection.books);
+
+  const borrowBook = (id) => {
+    dispatch({ type: "borrow_book", id });
+  };
+
+  const returnBook = (id) => {
+    dispatch({ type: "return_book", id });
+  };
+
+  const removeBook = (id) => {
+    dispatch({ type: "remove_book", id });
+  };
+
+  const addBook = (props) => {
+    dispatch({ type: "add_book", ...props });
+  };
+
+  return (
+    <LibraryContext.Provider
+      value={{ books, borrowBook, returnBook, removeBook, addBook }}
+    >
+      {children}
+    </LibraryContext.Provider>
+  );
+};
+
+export const useLibrary = () => {
+  const context = useContext(LibraryContext);
+  if (!context) {
+    throw new Error("useLibrary must be used within a LibraryContextProvider");
+  }
+  return context;
 };
