fc758a277af75eaa8ff3694f0f67a4abf1fdcdfb
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-01 22:48:54 +0200

    feat: Add user list, fetch data with useEffect and render the u-names

--- 12_react-advanced/07_01_talking-to-an-api_fetching-data/src/App.jsx
+++ 12_react-advanced/07_01_talking-to-an-api_fetching-data/src/App.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
-    return (
-        <div className="App">
-            <h1>React Hooks exercise starter</h1>
-        </div>
-    );
+  const [userList, setUserList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3003/users");
+      const json = await response.json();
+      console.log(json);
+      setUserList(json);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Blogging site exercise </h1>
+      <p>(Exercise for Module-12/07 - Talking to an API)</p>
+      <ul>
+        {userList.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
