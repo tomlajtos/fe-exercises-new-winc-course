ea40f794724d0368de229fb64599738e2a742bde
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-02 23:12:42 +0200

    feat: Show user detail when user is selected on click

--- 12_react-advanced/07_01_talking-to-an-api_fetching-data/src/App.jsx
+++ 12_react-advanced/07_01_talking-to-an-api_fetching-data/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
+import { UserDetail } from "./UserDetail";
 
 const App = () => {
   const [userList, setUserList] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("http://localhost:3003/users");
@@ -16,11 +18,20 @@ const App = () => {
     <div className="App">
       <h1>Blogging site exercise </h1>
       <p>(Exercise for Module-12/07 - Talking to an API)</p>
-      <ul>
-        {userList.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <div className="container">
+        <ul className="userList">
+          {userList.map((user) => (
+            <li
+              className="userListItem"
+              key={user.id}
+              onClick={() => setSelectedUser(user)}
+            >
+              {user.name}
+            </li>
+          ))}
+        </ul>
+        {selectedUser && <UserDetail user={selectedUser} />}
+      </div>
     </div>
   );
 };
