412b1149f862037c5e506adb8f258784c7c7bbc9
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-06 18:02:11 +0200

    Add createUsr function, render add user form

--- 12_react-advanced/07_03_talking-to-an-api_send-post-request/src/App.jsx
+++ 12_react-advanced/07_03_talking-to-an-api_send-post-request/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { UserDetail } from "./UserDetail";
+import { AddUserForm } from "./AddUserForm";
 
 const App = () => {
   const [userList, setUserList] = useState([]);
@@ -8,27 +9,51 @@ const App = () => {
     const fetchUsers = async () => {
       const response = await fetch("http://localhost:3003/users");
       const users = await response.json();
+      console.log(users);
       setUserList(users);
     };
     fetchUsers();
   }, []);
 
+  const createUser = async (user) => {
+    try {
+      const response = await fetch("http://localhost:3003/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+
+      const result = await response.json();
+      console.log("post successful", result);
+      user = { id: result.id, ...user };
+      console.log("new user:", user);
+      setUserList([...userList, user]);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Blogging site exercise </h1>
-      <p>(Exercise for Module-12/07 - Talking to an API)</p>
-      <div className="container">
-        <ul className="userList">
-          {userList.map((user) => (
-            <li
-              className="userListItem"
-              key={user.id}
-              onClick={() => setSelectedUser(user)}
-            >
-              {user.name}
-            </li>
-          ))}
-        </ul>
+      <div className="usersPane">
+        <h1>Blogging site exercise </h1>
+        <p>(Exercise for Module-12/07 - Talking to an API)</p>
+        <div className="container">
+          <ul className="userList">
+            {userList.map((user) => (
+              <li
+                className="userListItem"
+                key={user.id}
+                onClick={() => setSelectedUser(user)}
+              >
+                {user.name}
+              </li>
+            ))}
+          </ul>
+          <AddUserForm createUser={createUser} />
+        </div>
         {selectedUser && <UserDetail user={selectedUser} />}
       </div>
     </div>
