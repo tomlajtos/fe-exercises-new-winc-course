6d12af8a4bcad91312cfce9df2b44d95827ccd03
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-20 17:50:42 +0200

    refactor: use Root component as parent, add nested routes to children

--- 12_react-advanced/08_02_react-router_navigation-and-nested-routes/src/main.jsx
+++ 12_react-advanced/08_02_react-router_navigation-and-nested-routes/src/main.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import { Root } from "../Root";
 import { NewPost } from "../NewPost";
 import { Post } from "../Post";
 import { PostList } from "../PostList";
 import { User } from "../User";
 
 const router = createBrowserRouter([
-  { path: "/", element: <PostList /> },
-  { path: "/user/:userId", element: <User /> },
-  { path: "/post/:postId", element: <Post /> },
-  { path: "/post/new", element: <NewPost /> },
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      // PostList has the same path as its parent, Root '/' >>> PostList
+      // will render in place of Outlet in Root by default
+      { path: "/", element: <PostList /> },
+      { path: "/user/:userId", element: <User /> },
+      { path: "/post/:postId", element: <Post /> },
+      { path: "/post/new", element: <NewPost /> },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("app")).render(
