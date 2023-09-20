a6df60f06f0d73d68cb067487021df7c7378078f
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-20 17:59:05 +0200

    fix: move components to /src dir, update imports accordingly

--- 12_react-advanced/08_02_react-router_navigation-and-nested-routes/src/main.jsx
+++ 12_react-advanced/08_02_react-router_navigation-and-nested-routes/src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import { Root } from "../Root";
-import { NewPost } from "../NewPost";
-import { Post } from "../Post";
-import { PostList } from "../PostList";
-import { User } from "../User";
+import { Root } from "./Root";
+import { NewPost } from "./NewPost";
+import { Post } from "./Post";
+import { PostList } from "./PostList";
+import { User } from "./User";
 
 const router = createBrowserRouter([
   {
