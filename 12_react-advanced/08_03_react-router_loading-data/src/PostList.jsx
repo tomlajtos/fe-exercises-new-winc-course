441f317d4805b5df76c512dd2d4d6cf62a589115
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-21 23:43:44 +0200

    Add loader function to fetch data and log data with useLoaderData

--- 12_react-advanced/08_03_react-router_loading-data/src/PostList.jsx
+++ 12_react-advanced/08_03_react-router_loading-data/src/PostList.jsx
@@ -1,7 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+
 export const PostList = () => {
+  const { users, posts } = useLoaderData();
+  console.log(users, posts);
+
   return (
     <>
       <div>post list</div>
     </>
   );
+};
+
+export const loader = async function () {
+  const users = await (await fetch("http://localhost:3003/users")).json();
+  const posts = await (await fetch("http://localhost:3003/posts")).json();
+
+  return { users, posts };
 };
