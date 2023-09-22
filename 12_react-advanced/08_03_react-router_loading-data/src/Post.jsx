5c0262c74173af592feaa236d0a1379fb3a63fe8
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-22 16:30:25 +0200

    Add basic Post comp JSX to be rendered

--- 12_react-advanced/08_03_react-router_loading-data/src/Post.jsx
+++ 12_react-advanced/08_03_react-router_loading-data/src/Post.jsx
@@ -1,7 +1,37 @@
+import { Link } from "react-router-dom";
+
 export const Post = () => {
   return (
-    <>
-      <div>post</div>
-    </>
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>
+        by{" "}
+        <Link to={`/user/${post.userId}`}>
+          {users.find((user) => user.id === post.userId).name}
+        </Link>
+      </p>
+      <p>{post.body}</p>
+      <hr />
+
+      {comments.length > 0 && (
+        <div className="comments">
+          <h2>Comments:</h2>
+          {comments.map((comment) => {
+            const commenter = users.find((user) => user.id === comment.userId);
+            return (
+              <div key={comment.id} className="comment">
+                <p>
+                  <Link to={`/user/${comment.userId}`}>
+                    {commenter.name} commented:
+                  </Link>
+                </p>
+                <p>{comment.comment}</p>
+                <hr />
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
   );
 };
