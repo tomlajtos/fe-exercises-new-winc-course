a0eccbc46b45856c197372a53837a1b92459cb1c
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-22 16:31:31 +0200

    Add basic User comp JSX to be rendered

--- 12_react-advanced/08_03_react-router_loading-data/src/User.jsx
+++ 12_react-advanced/08_03_react-router_loading-data/src/User.jsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
+
 export const User = () => {
   return (
-    <>
-      <div>user</div>
-    </>
+    <div className="user">
+      <h1>{user.name}</h1>
+
+      {posts.length > 0 && (
+        <>
+          <h2>Posts:</h2>
+          {posts.map((post) => (
+            <div key={post.id}>
+              <Link to={`/post/${post.id}`}>{post.title}</Link>
+            </div>
+          ))}
+        </>
+      )}
+
+      {comments.length > 0 && (
+        <>
+          <h2>Comments:</h2>
+          {comments.map((comment) => (
+            <div key={comment.id}>
+              <Link to={`/post/${comment.postId}`}>{comment.comment}</Link>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
   );
 };
