8b1de46fc176d580d43406c39efb52474802ddac
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-26 13:54:00 +0200

    Add basic JSX for the <Form /> to be rendered

--- 12_react-advanced/08_04_react-router_sending-data/src/NewPost.jsx
+++ 12_react-advanced/08_04_react-router_sending-data/src/NewPost.jsx
@@ -1,13 +1,28 @@
 // NOTE: no form validation or authentication will be done to keep the focus on the basics
 
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Form } from "react-router-dom";
 
 export const NewPost = () => {
   const { users } = useLoaderData();
   console.log(users);
   return (
     <>
-      <div>new post</div>
+      <Form method="post">
+        <span>Title:</span>
+        <input name="title"></input>
+        <span>Post:</span>
+        <textarea name="body" />
+        <span>Select a user:</span>
+        <select name="userId">
+          <option value={""}>Choose a user from the list bleow...</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Submit</button>
+      </Form>
     </>
   );
 };
